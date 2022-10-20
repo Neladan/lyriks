60a05d669505ba7202d8e51aea0fa6911ff16125
Lionel ADANKON <lionelad006@gmail.com>
2022-10-20 11:07:56 +0100

    Sidebar

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,5 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { Error, Loader, SongCard } from "../components";
 import { genres } from '../assets/constants';
 
@@ -5,6 +7,8 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 
 const Discover = () => {
+    const dispatch =  useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error  } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
 
@@ -32,6 +36,9 @@ const Discover = () => {
                     <SongCard 
                         key={song.key}
                         song={song}
+                        isplaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
                         i={i}
                     />
                 ))}
